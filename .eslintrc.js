748c57638ff5ff796511b90b8f1401390b725d71
권준동 <kwonjoondong@gmail.com>
2022-02-02 21:39:25 +0900

    [FEAT] #1 eslint/prettier 설정 * 파서: @typescript-eslint/parser * 플러그인: @typescript-eslint * prettier: 공백을 이용한 들여쓰기 4킨

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'prettier/prettier': [
+            'warn',
+            {
+                tabWidth: 4,
+            },
+        ],
+        'no-shadow': 0,
+        curly: 0,
+        'react-native/no-inline-styles': 0,
+    },
 };
