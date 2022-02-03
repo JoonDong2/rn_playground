46f23fc72438cfa911a7626e0b420ead492d0a73
권준동 <kwonjoondong@gmail.com>
2022-02-04 00:27:02 +0900

    [FEAT] #1 eslint jest 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     extends: '@react-native-community',
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'jest'],
     rules: {
         'prettier/prettier': [
             'warn',
@@ -14,5 +14,10 @@ module.exports = {
         'no-shadow': 0,
         curly: 0,
         'react-native/no-inline-styles': 0,
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
     },
 };
