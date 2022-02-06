17c09bf120efcbdce1360e91c12b4bce5c80d255
권준동 <kwonjoondong@gmail.com>
2022-02-06 19:08:47 +0900

    [FIX] #3 "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!" 경고 무시 설정

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import StackNavigation from './navigations/StackNavigation';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
 
 export default () => {
     return (
