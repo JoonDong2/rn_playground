6b32136fd6f77140cce8ccef1078039362102f82
권준동 <kwonjoondong@gmail.com>
2022-02-16 01:10:14 +0900

    [FIX] #5 서버에 올린 joom-server와 연결

--- Constants/index.tsx
+++ Constants/index.tsx
@@ -6,6 +6,6 @@ export const screen = Dimensions.get('screen');
 export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
 
-const END_POINT = 'https://4c54-2001-4430-5063-c238-104d-475-30b5-abdf.ngrok.io';
+const END_POINT = 'https://joom.joon-lab.com';
 export const CHAT_LIST_END_POINT = END_POINT + '/webrtc/rooms';
 export const WS_END_POINT = END_POINT + '/webrtc/socket';
